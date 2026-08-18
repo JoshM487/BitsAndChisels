cf2cc7323a5137ce6f352c9afa4d9e82b27cc73b
JoshM487 <89715250+JoshM487@users.noreply.github.com>
2026-08-18 19:16:57 -0400

    Cache one clipped mesh per chiseled block

--- src/main/java/io/github/coolmineman/bitsandchisels/client/BitsBlockRenderState.java
+++ src/main/java/io/github/coolmineman/bitsandchisels/client/BitsBlockRenderState.java
@@ -2,11 +2,8 @@ package io.github.coolmineman.bitsandchisels.client;
 
 import net.minecraft.client.renderer.block.BlockModelRenderState;
 import net.minecraft.client.renderer.blockentity.state.BlockEntityRenderState;
-import java.util.ArrayList;
-import java.util.List;
 
 public final class BitsBlockRenderState extends BlockEntityRenderState {
-    public final List<MicroBit> visibleBits = new ArrayList<>();
-
-    public record MicroBit(int x, int y, int z, BlockModelRenderState model) {}
+    public final BlockModelRenderState model = new BlockModelRenderState();
+    public long meshRevision = Long.MIN_VALUE;
 }
